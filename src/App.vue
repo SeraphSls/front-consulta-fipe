22995abfae19948e4e9eaadc867cb74d231188e4
Guilherme <guilherme.a.inacio@outlook.com>
2023-04-25 19:56:41 -0300

    Criacao de nav

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
 import navComponent from './components/navComponent.vue'
 </script>
 
-<template>
+<template class="app">
 <body>
   <navComponent></navComponent>
+
 </body>
 </template>
+
+<style>
+ #body{
+  background-color: #233A54 !important;
+ }
+</style>
 
--- src/components/navComponent.vue
+++ src/components/navComponent.vue
@@ -3,48 +3,64 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <div class="topnav" id="myTopnav">
-       
-        <a href="#home" class="active">
-            <img alt="Dragon" class="logo" src="@/assets/logotransparent.png" width="100" height="100"/>
-        </a>
+       <div id="logoDiv">
+          <img alt="Dragon" class="logo" src="@/assets/logotransparent.png" width="100" height="100"/>
+       </div>
+      <div class="buttons">
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
+      </div>
     </div>
 </template>
 
 <script>
-
     export default {
         
     }
 </script>
 <style>
+
+@font-face {
+   font-family: "Poppins-Light-Italic";
+   src: url("../assets/fonts/Poppins-LightItalic.ttf");
+}
+
+@font-face {
+   font-family: "Poppins-Regular";
+   src: url("../assets/fonts/Poppins-Regular.ttf");
+}
+
+@font-face {
+   font-family: "Poppins-Regular-Italic";
+   src: url("../assets/fonts/Poppins-Italic.ttf");
+}
 .topnav {
   overflow: hidden;
+  display: flex;
 }
 
-.topnav a {
-  float: left;
-  display: block;
+.buttons {
+  display: flex;
+  margin-left: 60%;
+}
+
+.buttons a {
+    height: 60px;
+    float: left;
+    display: block;
+    color: #04BBBA;
+    text-align: center;
+    padding: 1% 10%;
+    text-decoration: none;
+    font-size: 38px;
+    font-family: 'Poppins-Regular-Italic';
+}
+
+.buttons a:hover {
+  background-color: #233A54;
   color: #04BBBA;
-  text-align: center;
-  padding: 1% 10%;
-  text-decoration: none;
-  font-size: 17px;
+  border-radius: 25%;
 }
 
-.topnav a:hover {
-  background-color: #B79370;
-  color: #04BBBA;
-}
-
-.topnav a.active.logo {
-  color: #B79370;
-  position: absolute;
-}
-
-.topnav logo {
-  color: #B79370;
-}
 </style>
